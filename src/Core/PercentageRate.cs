d4dace7ba8811f54b953ba59c5ba4735aaeb9628
LaBani Said <labani.said@outlook.com>
2025-01-14 17:39:24 +0100

    Add PercentageRate.cs to Core

---
+++ src/Core/PercentageRate.cs
@@ -0,0 +1,63 @@
+﻿namespace BankAsPlatform;
+
+using System;
+using System.Globalization;
+
+public readonly struct PercentageRate
+{
+    private const int MaxTotalDigits = 11;
+    private const int MaxFractionalDigits = 10;
+    private const decimal MaxValue = 1000m;
+    private const decimal MinValue = 0m;
+
+    public decimal Value { get; }
+
+    public PercentageRate(decimal value)
+    {
+        if (value < MinValue || value > MaxValue)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), "The percentage rate must be between 0 and 1000.");
+        }
+
+        string valueString = value.ToString("0.####################", CultureInfo.InvariantCulture);
+        int totalDigits = valueString.Replace(".", "").Length;
+        int fractionalDigits = valueString.Contains(".") ? valueString.Split('.')[1].Length : 0;
+
+        if (totalDigits > MaxTotalDigits)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), "The total number of digits (before and after the decimal) must not exceed 11.");
+        }
+
+        if (fractionalDigits > MaxFractionalDigits)
+        {
+            throw new ArgumentOutOfRangeException(nameof(value), "The number of fractional digits must not exceed 10.");
+        }
+
+        // Always round to the correct number of fractional digits
+        Value = decimal.Round(value, MaxFractionalDigits, MidpointRounding.AwayFromZero);
+    }
+
+    // Factory method to create a PercentageRate instance from a decimal value
+    public static PercentageRate FromDecimal(decimal value)
+    {
+        return new PercentageRate(value);
+    }
+
+    // Override ToString() method to display the value in a string format
+    public override string ToString()
+    {
+        return Value.ToString("0.####################", CultureInfo.InvariantCulture);
+    }
+
+    // Implicit conversion to decimal (useful for calculations)
+    public static implicit operator decimal(PercentageRate rate)
+    {
+        return rate.Value;
+    }
+
+    // Example of how you might use the value (i.e., to calculate percentage of another value)
+    public decimal Calculate(decimal baseValue)
+    {
+        return baseValue * Value / 100;
+    }
+}
