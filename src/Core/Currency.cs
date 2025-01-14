eb9e3592906c18617e3129bfe57e2e31b9f21de2
LaBani Said <labani.said@outlook.com>
2025-01-14 16:20:34 +0100

    Add Currency to Core

---
+++ src/Core/Currency.cs
@@ -0,0 +1,14 @@
+﻿namespace BankAsPlatform;
+
+// WIP
+// Explicit and compliant with ISO 4217
+public sealed record Currency
+{
+    private Currency(string AlphabeticCode, int NumericCode, int MinorUnit, string Entity) { }
+
+    public readonly static Currency USD = new ("USD", 840, 2, "United States of America");
+
+    public readonly static Currency EUR = new ("EUR", 978, 2, "European Union");
+
+    public readonly static Currency JPY = new("JPY", 392, 0, "Japan");
+}
